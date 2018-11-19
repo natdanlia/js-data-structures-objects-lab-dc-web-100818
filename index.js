b1d96e0fe3e2144d74b8f484bc55cdd3f5320a7d
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2018-11-18 21:54:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+let driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  let newObject = Object.assign({}, driver, {[key]:value})
+  return newObject
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value
+    return driver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+  let newObject = Object.assign({},driver)
+  delete newObject[key]
+  return newObject
+}
